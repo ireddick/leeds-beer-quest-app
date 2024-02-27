e373bb84e64a8b3710922148d85ae6a4a56f2b51
Iain Reddick <iain.reddick@gmail.com>
2024-02-27 17:08:56 +0000

    Fetch and display list of locations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,32 @@
-const App = () => <p>Hello World!</p>
+import { useState, useEffect } from "react"
 
-export default App
+interface Location {
+  name: string
+}
+
+export default function App() {
+  const [locations, setLocations] = useState<Location[]>([])
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      const response = await fetch("/api/locations")
+
+      const locations: Location[] = (await response.json()).data
+
+      setLocations(locations)
+    }
+
+    fetchLocations()
+  })
+
+  return (
+    <div>
+      <h1>Pubs 🍺</h1>
+      <ul>
+        {locations.map(location => (
+          <li key={location.name}>{location.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
