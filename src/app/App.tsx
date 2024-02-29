b43917f799e81806b2e3cd6ebe01a4ee2422071b
Iain Reddick <iain.reddick@gmail.com>
2024-02-29 14:30:53 +0000

    Get users location or use default

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,26 +2,43 @@ import { useState, useEffect } from "react"
 import styles from "./App.module.css";
 
 export default function App() {
-  const [locations, setLocations] = useState<Location[]>([])
+  const [location, setLocation] = useState<Coord>(LEEDS_CITY_CENTRE)
+  const [venues, setVenues] = useState<Venue[]>([])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        })
+      },
+      (_) => {
+        setLocation(LEEDS_CITY_CENTRE)
+      }
+    );
+  }, [])
 
   useEffect(() => {
     const fetchLocations = async () => {
       const response = await fetch("/api/locations")
 
-      const locations: Location[] = (await response.json()).data
+      const locations: Venue[] = (await response.json()).data
 
-      setLocations(locations)
+      setVenues(locations)
     }
 
     fetchLocations()
-  }, [])
+  }, [location])
 
   return (
     <div>
       <h1 className={styles.title}>Leeds Pub Finder 🍺</h1>
 
+      <p>Venues near {location.lat}, {location.lng}</p>
+
       <ul className={styles.locations}>
-        {locations.map(location => (
+        {venues.map(location => (
           <li className={styles.location} key={location.name}>
             <div className={styles.details}>
               <h2 className={styles.name}>{location.name}</h2>
@@ -45,7 +62,7 @@ export default function App() {
   )
 }
 
-interface Location {
+interface Venue {
   name: string
   category: string
   url: string // likely ought to be a URL
@@ -54,7 +71,7 @@ interface Location {
   thumbnail: string // likely ought to be a URL
   lat: number
   lng: number
-  address: string,
+  address: string
   phone: string
   twitter: string
   stars_beer: number
@@ -63,3 +80,10 @@ interface Location {
   stars_value: number
   tags: string[]
 }
+
+interface Coord {
+  lat: number
+  lng: number
+}
+
+const LEEDS_CITY_CENTRE: Coord = { lat: 53.79648, lng: -1.54785 }
